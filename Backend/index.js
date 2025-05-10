f87cfcb68042a37a80181a656fcfb7d55ef967f1
23f2002092 <23f2002092@ds.study.iitm.ac.in>
2025-05-10 22:09:30 +0530

    ignore test

--- Backend/index.js
+++ Backend/index.js
@@ -1,20 +1,22 @@
-const express = require('express');
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 3000;
-const {connectDb} = require('./db/db.js')
-const userRoute = require('./routes/user.routes.js')
+const { connectDb } = require("./db/db.js");
+const userRoute = require("./routes/user.routes.js");
+
+console.log("tesiting");
 
 app.use(express.json());
-app.use(cookieParser())
-app.use(express.urlencoded({extended:true}))
-app.use(cors())
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
-app.use('/', userRoute)
+app.use("/", userRoute);
 
-app.listen(PORT,()=>{
-    console.log('Server is listening on PORT',PORT);
-    connectDb()
-})
+app.listen(PORT, () => {
+  console.log("Server is listening on PORT", PORT);
+  connectDb();
+});
